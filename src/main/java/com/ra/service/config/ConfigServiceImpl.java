3101fb5e906691b099f572f6df13b982c064bb93
tanquynh <tanquynh1605@gmail.com>
2024-05-30 08:32:58 +0700

    resolverConflig

--- src/main/java/com/ra/service/config/ConfigServiceImpl.java
+++ src/main/java/com/ra/service/config/ConfigServiceImpl.java
@@ -1,6 +1,6 @@
 package com.ra.service.config;
 
-<<<<<<< HEAD
+
 import com.ra.exception.CustomException;
 import com.ra.model.dto.mapper.HttpResponse;
 import com.ra.model.dto.mapper.PageDataDTO;
@@ -168,12 +168,5 @@ private IConfigRepository configRepository;
                 "ConfigStatus change successfully !!\""
         ), HttpStatus.OK);
     }
-=======
-import org.springframework.stereotype.Service;
 
-@Service
-public class ConfigServiceImpl implements IConfigService {
-
-
->>>>>>> 0d7ce33144a48cd35cc8f29161187665f503c3bc
 }
